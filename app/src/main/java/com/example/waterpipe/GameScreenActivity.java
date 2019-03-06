353c1941cd0eef89be340b9cc67369c563f29b2c
Arran <Arran4567@gmail.com>
2019-03-06 20:14:49 +0000

    Added a confirmation box to back out of game screen. Added a Pipe object file with constructor, getters and setters.

--- app/src/main/java/com/example/waterpipe/GameScreenActivity.java
+++ app/src/main/java/com/example/waterpipe/GameScreenActivity.java
@@ -1,17 +1,35 @@
 package com.example.waterpipe;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.TextView;
+import android.view.View;
 
 public class GameScreenActivity extends AppCompatActivity {
+
+    private SystemClock systemClock;
+    private TextView tvTime;
+
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
         int difficulty = getIntent().getIntExtra("difficulty", -1);
-        TextView tv = findViewById(R.id.title);
+        TextView tv = findViewById(R.id.tvTitle);
+        tvTime = findViewById(R.id.tvTime);
         switch (difficulty){
             case 0:
                 tv.setText("Beginner");
@@ -26,5 +44,54 @@ public class GameScreenActivity extends AppCompatActivity {
                 throw new RuntimeException("Unknown button ID");
         }
 
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+            tvTime.setText("Time: " + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            customHandler.postDelayed(this, 0);
+        }
+    };
+
+    public void stopTimer(View view){
+        timeSwapBuff += timeInMilliseconds;
+        customHandler.removeCallbacks(updateTimerThread);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you wish to go back? Your game data will be lost.")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do nothing
+                            return;
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/example/waterpipe/Pipe.java
@@ -0,0 +1,31 @@
+package com.example.waterpipe;
+
+public class Pipe {
+    private boolean bend;
+    private int direction;
+
+    public Pipe(){
+        bend = getRandomBoolean();
+        direction = (int)(Math.random()*4);
+    }
+
+    public static boolean getRandomBoolean() {
+        return Math.random() < 0.5;
+    }
+
+    public boolean getBend(){
+        return bend;
+    }
+
+    public int getDirection(){
+        return direction;
+    }
+
+    public void setBend(boolean newBend){
+        this.bend = newBend;
+    }
+
+    public void setDirection(int newDirection){
+        this.direction = newDirection;
+    }
+}
